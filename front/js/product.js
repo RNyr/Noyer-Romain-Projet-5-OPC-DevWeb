90bbb80957672c5adcf7bb089d4a0f2cccaadebf
Noyer Romain <romainnoyer1995@gmail.com>
2022-11-24 17:41:05 +0100

    LocalStorage

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,5 @@
-let params = new URL(window.location.href).searchParams;
-let newID = params.get("id");
+let url = new URL(window.location.href);
+let productId = url.searchParams.get("id");
 
 const image = document.querySelector(".item__img");
 const title = document.getElementById("title");
@@ -7,17 +7,40 @@ const price = document.getElementById("price");
 const description = document.getElementById("description");
 const colors = document.getElementById("colors");
 
-fetch("http://localhost:3000/api/products/" + newID)
-  .then((res) => res.json())
-  .then((data) => {
-    image.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-    title.innerHTML = `<h1>${data.name}</h1>`;
-    price.innerText = `${data.price}`;
-    description.innerText = `${data.description}`;
+fetch("http://localhost:3000/api/products/" + productId)
+  .then((res) => {
+    if (res.ok) {
+      res.json().then((data) => {
+        image.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+        title.innerHTML = `<h1>${data.name}</h1>`;
+        price.innerText = `${data.price}`;
+        description.innerText = `${data.description}`;
+
+        for (number in data.colors) {
+          colors.options[colors.options.length] = new Option(
+            data.colors[number],
+            data.colors[number]
+          );
+        }
+      });
+    }
   })
-  //--------------Message d'erreur----------------
+
   .catch((_error) => {
     alert("Le serveur ne répond pas.");
   });
 
-// appeller une fois le fetch dans une variable, "variable globale"
+////-------------LocalStorage---------
+
+const quantity = document.getElementById("quantity");
+const addToCart = document.getElementById("addToCart");
+
+addToCart.addEventListener("click", () => {
+  let choice = {
+    id: productId,
+    color: colors.value,
+    quantity: quantity.value,
+  };
+  let saveChoice = JSON.stringify(choice);
+  localStorage.setItem("Cart", saveChoice);
+});
--- front/js/script.js
+++ front/js/script.js
@@ -20,11 +20,11 @@ function showProducts(data) {
 
     itemCard.innerHTML += `
         <a href="./product.html?id=${product._id}">
-        <article>
-          <img src="${product.imageUrl}" alt="${product.altTxt}">
-          <h3 class="productName">${product.name}</h3>
-          <p class="productDescription">${product.description}</p>
-        </article>
+          <article>
+           <img src="${product.imageUrl}" alt="${product.altTxt}">
+           <h3 class="productName">${product.name}</h3>
+           <p class="productDescription">${product.description}</p>
+           </article>
         </a>
       `;
   }
